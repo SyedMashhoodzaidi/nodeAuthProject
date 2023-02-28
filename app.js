19f21f9533c562187e370913d4e23c684d6fffbc
Syed Mashhood zaidi <syedmashhoodzaidi@gmail.com>
2023-02-28 07:02:34 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth } = require('./middleware/authMiddleware');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://Mashhood:Mashhood@cluster0.bsurkl9.mongodb.net/UserAuth?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => {
+    app.listen(3000);
+    console.log("Connected To DataBase::::::::::");
+    console.log("Listening Now............");
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
